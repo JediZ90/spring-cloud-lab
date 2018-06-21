846ba591a4ecf91b38bd8a27550c14c16b82cb80
Zhang Baohao <imbh.zhang@gmail.com>
2018-06-21 21:42:42 +0800

    update

--- spring-cloud-lab-echo/src/main/java/lab/cloud/echo/EchoServerApplication.java
+++ spring-cloud-lab-echo/src/main/java/lab/cloud/echo/EchoServerApplication.java
@@ -3,10 +3,12 @@ package lab.cloud.echo;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class EchoServerApplication {
 
---
+++ spring-cloud-lab-eureka/README.md
@@ -0,0 +1,5 @@
+# spring-cloud-lab-eureka
+
+参考文档：https://spring.io/guides/gs/service-registration-and-discovery/
+
+http://localhost:5002/
